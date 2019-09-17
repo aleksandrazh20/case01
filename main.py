67f1c23e1ef874aecc44e2816b8dff91f44b44e1
aleksandrazh20 <hollandjoe20@gmail.com>
2019-09-17 20:43:45 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-#case1
+# case1
+# Vlasov V, Anufrieva A, Zhuravleva A.
+def triangle_type_1(x, y, a, color) :
+    # TODO: (Vlas) Paint a triangle.
+    pass
+def triangle_type_2(x, y, a, color) :
+    # TODO: (Nastya) Paint a triangle.
+    pass
+def triangle_type_3(x, y, a, color):
+    # TODO: (Sasha) Paint a triangle.
+    pass
+def triangle_type_4(x, y, a, color) :
+    # TODO: (Vlas) Paint a triangle.
+    pass
+def triangle_type_5(x, y, a, color) :
+    # TODO: (Nastya) Paint a triangle.
+    pass
+def rectangle_type_1(x, y, a, color) :
+    # TODO: (Sasha) Paint a rectangle.
+    pass
+def rectangle_type_2(x, y, a, color) :
+    # TODO: (Vlas) Paint a rectangle.
+    pass
+def square(x, y, a, color) :
+    # TODO: (Nastya) Paint a square.
+    pass
+def main() :
+    pass
+
